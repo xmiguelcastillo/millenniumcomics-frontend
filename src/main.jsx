985013772d0e5e65dfb19c210f2d8f35308ce8e4
xmiguelcastillo <xmiguelcastillo@gmail.com>
2025-11-13 16:56:35 -0500

    Setup GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
